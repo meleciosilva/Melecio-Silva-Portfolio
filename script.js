c8518498fc8ca43b7ddd1f5ec54277fb63b0bcce
meleciosilva <mpsilva94@gmail.com>
2021-02-23 18:30:32 -0500

    updated greeting function

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ function getTime() {
 // Retrieves appropriate greeting respective to time of day
 function getGreeting() {  
   const time = getTime();
-  console.log(time);
   if (time < 12) {
     return "Good Morning! My Name is Melecio.";
-  } else if (time >= 12 || time <= 18) {
+  } else if (time >= 12 && time < 18) {
     return "Good Afternoon! My name is Melecio.";
-  } else if (time > 18) {
+  } else if (time >= 18) {
       return "Good Evening! My name is Melecio.";
   } else {
     return "Hi! My Name is Melecio"
