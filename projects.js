a6a9806ad6286f48c0d18afaa9914fda1000bb2a
meleciosilva <mpsilva94@gmail.com>
2022-03-12 15:18:27 -0600

    update breweries description

--- projects.js
+++ projects.js
@@ -1,11 +1,12 @@
-export let projects = 
-[
+export let projects = [
   {
     id: 1,
     name: "Restaurant Reservations",
-    description: "A restaurant reservations platform designed to manage booking, seating, and managing reservations.",
+    description:
+      "A restaurant reservations platform designed to manage booking, seating, and managing reservations.",
     github_url: "https://github.com/meleciosilva/reservations",
-    live_url: "https://restaurant-reservation-frontend-gilt.vercel.app/dashboard?date=2020-12-30",
+    live_url:
+      "https://restaurant-reservation-frontend-gilt.vercel.app/dashboard?date=2020-12-30",
     project_image: "images/project-images/reservations.png",
     technology: [
       "<p>Built with <strong>Bootstrap</strong>, <strong>React</strong>, and <strong>Express</strong></p>",
@@ -16,20 +17,22 @@ export let projects =
   {
     id: 2,
     name: "Flashcard-o-matic",
-    description: "An online learning tool designed to help students study course content with flashcards.",
+    description:
+      "An online learning tool designed to help students study course content with flashcards.",
     github_url: "https://github.com/meleciosilva/flashcard-o-matic",
     live_url: "https://flashcard-o-matic-peach.vercel.app",
     project_image: "images/project-images/flashcards.png",
     technology: [
       "<p>Built with <strong>React</strong> and <strong>Bootstrap</strong> to develop an interactive user interface and experience</p>",
       "<p>Implemented<strong> React Hooks</strong> and <strong>React Router</strong> to manage API calls and component routing</p>",
-      "<p><strong>Express</strong> to build server and routes</p>"
+      "<p><strong>Express</strong> to build server and routes</p>",
     ],
   },
   {
     id: 3,
     name: "We Love Movies",
-    description: "A movie theater platform designed to help users navigate movies and showing theaters.",
+    description:
+      "A movie theater platform designed to help users navigate movies and showing theaters.",
     github_url: "https://github.com/meleciosilva/we-love-movies",
     live_url: "",
     project_image: "images/project-images/movies.png",
@@ -42,40 +45,44 @@ export let projects =
   {
     id: 4,
     name: "Event Manager",
-    description: "An Express RESTful API built with a series of routes to manage users and access or manipulate data for events.",
+    description:
+      "An Express RESTful API built with a series of routes to manage users and access or manipulate data for events.",
     github_url: "https://github.com/meleciosilva/event-management",
     live_url: "",
     project_image: "images/project-images/coding.jpg",
     technology: [
       "<p>Set up database with <strong>MongoDB</strong> to store information</p>",
       "<p>Implemented <strong>Mongoose</strong> library to model and manage application data</p>",
-      "<p>Implemented user authentication and authentication with <strong>JSON web token</strong></p>"
+      "<p>Implemented user authentication and authentication with <strong>JSON web token</strong></p>",
     ],
   },
   {
     id: 5,
     name: "VA Breweries",
-    description: "A directory of breweries, cideries, and craft beer bottle shops located in Virginia.",
+    description:
+      "A directory of breweries, brewhubs, cideries, and craft beer bottle shops located in Virginia.",
     github_url: "https://github.com/meleciosilva/va-breweries",
     live_url: "https://va-breweries.vercel.app/",
     project_image: "images/project-images/breweries.png",
     technology: [
       "<p>Built with <strong>Bootstrap</strong> to create layout, styling, and responsive design</p>",
+      "<p>Developed pagination, search filtering, and sorting with <strong>JavaScript</strong></p>",
       "<p>Implemented <strong>Axios</strong> to send HTTP requests to the Open Brewery Database</p>",
-      "<p>Used <strong>DOM manipulation</strong> to customize event listeners and rendering</p>"
+      "<p>Used <strong>DOM manipulation</strong> to customize event listeners and rendering</p>",
     ],
   },
   {
     id: 6,
     name: "Decoder Ring",
-    description: "A set of ciphers used to encode and decode messages (Caesar's Shift, Polybius Square, and Substitution).",
+    description:
+      "A set of ciphers used to encode and decode messages (Caesar's Shift, Polybius Square, and Substitution).",
     github_url: "https://github.com/meleciosilva/project-decoder-ring",
     live_url: "https://meleciosilva.github.io/project-decoder-ring",
     project_image: "images/project-images/decoder.png",
     technology: [
       "<p>Built using <strong>JavaScript</strong></p>",
       "<p>Implemented unit tests using <strong>Mocha</strong> and <strong>Chai</strong> to ensure functionality and minimize bug development</p>",
-      "<p>Created several helper functions to modulate code and improve readability</p>"
+      "<p>Created several helper functions to modulate code and improve readability</p>",
     ],
   },
-]
+];
